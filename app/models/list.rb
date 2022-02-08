f7f53cd366d0783c3fdb46fcbfa23ba21658456b
Mylene Desroses <mylene.desroses@gmail.com>
2022-02-08 10:34:40 -0400

    validations and  associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,10 @@
 class List < ApplicationRecord
+
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+  has_one_attached :cover
+
+  # validations
+  validates :title, presence: true, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+  # validations
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+
 end
